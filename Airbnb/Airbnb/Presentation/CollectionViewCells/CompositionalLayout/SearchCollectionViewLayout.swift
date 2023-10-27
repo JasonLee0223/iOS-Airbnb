f89d7cffff85486b029d64f4893a7eebe0ab1334
Jasonlee0223 <geonhaeng94@gmail.com>
2023-10-27 13:09:19 +0900

    [Style]: SearchCollectionView의 CompositionalLayout 수정
    - Inset 및 group height Size 수정

--- Airbnb/Airbnb/Presentation/CollectionViewCells/CompositionalLayout/SearchCollectionViewLayout.swift
+++ Airbnb/Airbnb/Presentation/CollectionViewCells/CompositionalLayout/SearchCollectionViewLayout.swift
@@ -10,10 +10,8 @@ import UIKit
 struct SearchCollectionViewLayout {
     
     func createSearchLayout() -> NSCollectionLayoutSection? {
-        let itemInset: CGFloat = 2.5
-        
         let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0 / 2.0))
-        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalWidth(1.0 / 2.0))
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalWidth(1.0 / 2.5))
         let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalWidth(1.0 / 5.0))
         
         let item = NSCollectionLayoutItem(layoutSize: itemSize)
@@ -22,8 +20,8 @@ struct SearchCollectionViewLayout {
         let header = NSCollectionLayoutBoundarySupplementaryItem(
             layoutSize: headerSize, elementKind: UICollectionView.elementKindSectionHeader, alignment: .top
         )
-        item.contentInsets = .init(top: itemInset, leading: itemInset, bottom: 16.0, trailing: itemInset)
-        section.contentInsets = .init(top: itemInset, leading: 16.0, bottom: itemInset, trailing: itemInset)
+        item.contentInsets = .init(top: 0.0, leading: 0.0, bottom: 16.0, trailing: 0.0)
+        section.contentInsets = .init(top: 0.0, leading: 16.0, bottom: 0.0, trailing: 0.0)
         section.boundarySupplementaryItems = [header]
         return section
     }
