48c35cacbe370cd24a3e5992231c93c5c8bcdb8f
Jasonlee0223 <geonhaeng94@gmail.com>
2023-10-31 16:58:43 +0900

    [Feat]: scrollView 레이아웃 설정

--- Airbnb/Airbnb/Presentation/ViewController/WishListViewController.swift
+++ Airbnb/Airbnb/Presentation/ViewController/WishListViewController.swift
@@ -22,6 +22,7 @@ final class WishListViewController: UIViewController {
     
     private let titleView: UILabel = UILabel()
     private let bottomLineView: UIView = UIView(frame: .zero)
+    private let scrollView = UIScrollView()
 //    private let wishItemImageView = WishItemImageView(frame: CGRect(x: 25, y: 150, width: 343.0, height: 240.0))
 //    private let wishItemView = WishInfoView(frame: CGRect(x: 25, y: 150, width: 343.0, height: 80))
     
@@ -32,17 +33,26 @@ final class WishListViewController: UIViewController {
         self.titleView.font = .systemFont(ofSize: 17, weight: .semibold)
         self.titleView.textColor = .AirbnbCustomColor.homeMaintitle.color
         self.titleView.sizeToFit()
+        self.bottomLineView.backgroundColor = .AirbnbCustomColor.bottomLineView.color
         self.navigationItem.titleView = self.titleView
-        
-        bottomLineView.backgroundColor = .AirbnbCustomColor.bottomLineView.color
         self.navigationController?.navigationBar.shadowImage = UIImage()
-        self.navigationController?.navigationBar.addSubview(bottomLineView)
+        self.scrollView.showsVerticalScrollIndicator = false
+        self.scrollView.showsHorizontalScrollIndicator = false
+        self.scrollView.backgroundColor = .red
     }
     
     private func setLayout() {
+        self.view.addSubview(self.bottomLineView)
         self.bottomLineView.snp.makeConstraints { make in
-            make.leading.trailing.bottom.equalToSuperview()
+            make.leading.trailing.equalTo(self.view)
+            make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top)
             make.height.equalTo(1.0)
+        }
+        
+        self.view.addSubview(self.scrollView)
+        self.scrollView.snp.makeConstraints { make in
+            make.centerX.centerY.width.equalToSuperview()
+            make.top.equalTo(self.bottomLineView.snp.bottomMargin).offset(40)
         }
 //        self.wishItemView.configureOfLabel(gradeText: "4.98", gradeCountText: "(후기 50개)")
 //        self.wishItemView.configureOfLabel(itemNameText: "비담집, 비우고담은집(집전체, 최대4인)", priceText: "₩308,571 / 박")
